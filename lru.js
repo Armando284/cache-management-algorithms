1fbdbddb5ede7678b5776958a80fc803ef762d8b
Armando <72764913+Armando284@users.noreply.github.com>
2024-09-06 13:28:24 -0400

    Feat: Adds remove by key to linked list

--- lru.js
+++ lru.js
@@ -20,12 +20,18 @@ class DoublyLinkedList {
     this.head = null
     this.tail = null
   }
+
+  get _isListEmpty() {
+    return this.head === null && this.tail === null
+  }
+
   // Add node
   add(key, value) {
     // if list is empty
-    if (this.head === null && this.tail === null) {
+    if (this._isListEmpty) {
       this.head = new LinkedNode(key, value)
       this.tail = this.head
+      console.log('Node added', this.tail.ToString())
       return
     }
 
@@ -37,10 +43,58 @@ class DoublyLinkedList {
     this.tail.next = node
     // last element on list points to new node
     this.tail = node
+    console.log('Node added', this.tail.ToString())
   }
 
   // Remove node
-  remove(key) { }
+  remove(key) {
+    if (this._isListEmpty) {
+      return
+    }
+
+    let node = this.head
+
+    // Head case
+    if (this.head && this.head.key === key) {
+      // Clean head next prev value
+      this.head.next.prev = null
+      // Makes head next the new head
+      this.head = this.head.next
+      console.log('Node removed', node.ToString())
+      return
+    }
+
+    // Tail case
+    if (this.tail && this.tail.key === key) {
+      node = this.tail // just for loggin
+      // Clean tail prev next value
+      this.tail.prev.next = null
+      // Makes tail prev the new tail
+      this.tail = this.tail.prev
+      console.log('Node removed', node.ToString())
+      return
+    }
+
+    node = this.head.next
+    do {
+      if (node.key === key) {
+        // prev node next value points to node next node
+        if (node.prev !== null) {
+          node.prev.next = node.next
+        }
+        // next node prev value points to node prev node
+        if (node.next !== null) {
+          node.next.prev = node.prev
+        }
+        // early exits from loop
+        console.log('Node removed', node.ToString())
+        return
+      }
+      node = node.next
+    } while (node && node.next !== null);
+
+    console.log('Node not found!')
+  }
 
   // Remove all nodes
   removeAll() {
@@ -72,3 +126,20 @@ class LRU {
 
   // Get data from cache
 }
+
+const testList = new DoublyLinkedList()
+testList.add(1, 'one')
+testList.add(2, 'two')
+testList.add(3, 'three')
+console.log(testList)
+console.log('------------')
+testList.remove(2)
+console.log(testList)
+console.log('------------')
+testList.remove(1)
+console.log(testList)
+console.log('------------')
+testList.removeAll()
+console.log(testList)
+console.log('------------')
+
