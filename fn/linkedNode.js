0a17231c4f1d338f2bbbf3ce8937fda4e252d98b
Armando <72764913+Armando284@users.noreply.github.com>
2024-09-07 16:22:23 -0400

    FeatL Refactors linked node class and function implementation to keep key and value private and prev and next public

--- fn/linkedNode.js
+++ fn/linkedNode.js
@@ -1,22 +1,22 @@
-function LinkedNode(key, value) {
-  let [_prev, _next] = [null, null]
+/**
+ * Node with links for previous and next nodes.
+ * @param { number | string } key Node id for search.
+ * @param { any } value Node value.
+ */
+export default function LinkedNode(key, value) {
+  let [prev, next] = [null, null]
+
   return {
-    get key() {
-      return key
-    },
-    get value() {
-      return value
-    },
-    get prev() { return _prev },
-    set prev(newPrev) {
-      _prev = newPrev
-      return _prev
-    },
-    get next() { return _next },
-    set next(newNext) {
-      _next = newNext
-      return _next
-    },
+    get key() { return key },
+    get value() { return value },
+    prev,
+    next,
+    /**
+     * Overrides object toString method.
+     * Allows callback for more customization.
+     * @param { ( value: any ) => void } callback 
+     * @returns 
+     */
     toString(callback) {
       return callback === undefined ? `{${key}: ${value}}` : callback(key, value)
     }
